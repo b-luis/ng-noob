f7ca796a06ceb0ecbacaf476ee0e017110e0b588
b-luis <bscarletluis@gmail.com>
2024-11-16 16:47:14 +0800

    refactor: change selectedUser into a signal, remove getter func and replace it as a computed signal

--- essentials/project/src/app/user/user.component.ts
+++ essentials/project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,5 +11,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath(): string {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //? without using signals
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    //? using signals
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
