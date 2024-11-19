b65712bdcbd2e2af101d3a9888c5208402ab0a09
b-luis <bscarletluis@gmail.com>
2024-11-19 12:14:20 +0800

    update: remove files and move them to specific folders and update app component and styles

--- essentials/project/src/app/app.component.ts
+++ essentials/project/src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, effect, signal } from '@angular/core';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { UserComponent } from './features/user/user.component';
+import { DUMMY_USERS } from './shared/data/dummy-users';
+import { TasksComponent } from './features/tasks/tasks.component';
+import { User } from './shared/models/user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS;
+  users: User[] = DUMMY_USERS;
+  selectedUserId = signal<string>('');
+
+  selectedUser = computed(() =>
+    this.users.find((user) => user.id === this.selectedUserId())
+  );
+
+  constructor() {
+    effect(() => {
+      console.log('Selected user:', this.selectedUser()?.name);
+    });
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId.set(id);
+  }
 }
