9db7540cd96633fa15442c657159ddfc8bb4ec7c
b-luis <bscarletluis@gmail.com>
2024-12-06 14:42:47 +0800

    fix(type): add type function

--- essentials/2-investment-calculator-project/src/investment-results.ts
+++ essentials/2-investment-calculator-project/src/investment-results.ts
@@ -2,7 +2,12 @@
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
 
-function calculateInvestmentResults() {
+function calculateInvestmentResults(
+  initialInvestment: any,
+  annualInvestment: any,
+  expectedReturn: any,
+  duration: any
+) {
   const annualData = [];
   let investmentValue = initialInvestment;
 
