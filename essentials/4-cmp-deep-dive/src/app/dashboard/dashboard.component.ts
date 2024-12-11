37e463847dbd1c0caee60d3b325f040fe532d905
b-luis <bscarletluis@gmail.com>
2024-12-11 16:03:32 +0800

    feat(component): add dashboard component that outputs several components

---
+++ essentials/4-cmp-deep-dive/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ServerStatusComponent } from './components/server-status/server-status.component';
+import { TrafficComponent } from './components/traffic/traffic.component';
+import { SupportTicketsComponent } from './components/support-tickets/support-tickets.component';
+import { DashboardItemComponent } from './components/dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    ServerStatusComponent,
+    TrafficComponent,
+    SupportTicketsComponent,
+    DashboardItemComponent,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  currentStatus = 'online';
+}
---
+++ essentials/4-cmp-deep-dive/src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  public dummyTrafficData = signal([
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ]);
+
+  constructor() {}
+}
