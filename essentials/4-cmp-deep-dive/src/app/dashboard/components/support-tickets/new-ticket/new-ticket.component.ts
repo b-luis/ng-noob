c3665160827ea25724eac73073c61b2892da062a
b-luis <bscarletluis@gmail.com>
2024-12-27 21:24:36 +0800

    update(new-ticket): add template variable for vie w child functionality both signal based and old way

--- essentials/4-cmp-deep-dive/src/app/dashboard/components/support-tickets/new-ticket/new-ticket.component.ts
+++ essentials/4-cmp-deep-dive/src/app/dashboard/components/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/button/button.component';
 import { ControlComponent } from '../../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,8 +11,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(title: HTMLInputElement) {
+  // @ViewChild('form')
+  // form?: ElementRef<HTMLFormElement>;
+
+  //? without viewchild decorator, but as a function
+  //? signal based viewchild
+  //! available only on versions 17.3 above
+  //? viewChildren()
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  //? important variation of viewchild incase you need to select multiple instances of viewchild items
+  // @ViewChildren(ButtonComponent) buttons
+
+  onSubmit(title: string, ticketText: string) {
     console.log('SUBMITTED!');
     console.log(title);
+    console.log(ticketText);
+
+    this.form().nativeElement.reset();
   }
 }
