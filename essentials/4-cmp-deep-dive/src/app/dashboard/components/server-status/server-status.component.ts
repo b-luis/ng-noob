6ebb025f024df94591e33352f1b3b14fa1c711d1
b-luis <bscarletluis@gmail.com>
2024-12-28 17:20:35 +0800

    refactor(server-status): put the interval side effect inside the effect() function

--- essentials/4-cmp-deep-dive/src/app/dashboard/components/server-status/server-status.component.ts
+++ essentials/4-cmp-deep-dive/src/app/dashboard/components/server-status/server-status.component.ts
@@ -1,10 +1,12 @@
 import {
   Component,
   DestroyRef,
+  effect,
   inject,
   input,
   OnDestroy,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -15,29 +17,36 @@ import {
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
   // private interval?: ReturnType<typeof setInterval>;
 
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    //? angular does not set up a subscription here
+    // console.log(this.currentStatus());
 
-  ngOnInit() {
-    const interval = setInterval(() => {
-      // 0 - 0.9999999
-      const rnd = Math.random();
+    //? workaround to execute effects
+    effect(() => {
+      const status = this.currentStatus();
+      console.log(`Server status changed to: ${status}`);
 
-      rnd < 0.5
-        ? (this.currentStatus = 'online')
-        : rnd < 0.9
-        ? (this.currentStatus = 'offline')
-        : (this.currentStatus = 'unknown');
-    }, 5000);
+      const interval = setInterval(() => {
+        const statuses = ['online', 'offline', 'unknown'];
+        const randomStatus =
+          statuses[Math.floor(Math.random() * statuses.length)];
+        this.currentStatus.set(
+          randomStatus as 'online' | 'offline' | 'unknown'
+        );
+      }, 5000);
 
-    this.destroyRef.onDestroy(() => {
-      clearInterval(interval);
+      this.destroyRef.onDestroy(() => {
+        clearInterval(interval);
+      });
     });
   }
+
+  ngOnInit() {}
 
   ngAfterViewInit() {
     console.log('AFTER VIEW INIT');
