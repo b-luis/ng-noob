57be3b4cc3219b2b4da329376ddb1df1e02a1826
b-luis <bscarletluis@gmail.com>
2024-12-28 14:37:01 +0800

    update(new-ticket): add #input variable on new-ticket template to access the element ref on the control component using contentChild() and add ngAfterContentInit hook to verify

--- essentials/4-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ essentials/4-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,6 +1,11 @@
 import {
+  AfterContentInit,
   Component,
+  contentChild,
+  ContentChild,
+  ElementRef,
   HostBinding,
+  inject,
   input,
   ViewEncapsulation,
 } from '@angular/core';
@@ -14,19 +19,38 @@ import {
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
-    // '(click)': 'onClick()',
+    '(click)': 'onClick()',
   },
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   //? for backward compatibility
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClick() {
   //    console.log('Clicked!');
   // }
 
-  label = input.required();
+  label = input.required<string>();
+  private el = inject(ElementRef);
 
-  // onClick() {
-  //   console.log('Clicked!');
-  // }
+  //? works like a viewchild
+  //? allows you to get a hold of the projected content, much like a viewchild but viewchild only allows you to access the content that is within the template with the template variable but cant giv u access on projected content (ng-content)
+  //? put #input variable in the template
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+
+  //? alternative way: Signal based
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit(): void {
+    console.log('AFTER CONTENT INIT');
+    console.log(this.control);
+  }
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control);
+  }
 }
