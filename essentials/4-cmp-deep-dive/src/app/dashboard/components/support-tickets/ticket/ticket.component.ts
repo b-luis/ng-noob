2721897d32a7d42eda41d6d0dc114514b7c9b95d
b-luis <bscarletluis@gmail.com>
2024-12-30 15:56:22 +0800

    update(ticket): add functions to handle completing and toggling the details of the ticket and update logic on the template

--- essentials/4-cmp-deep-dive/src/app/dashboard/components/support-tickets/ticket/ticket.component.ts
+++ essentials/4-cmp-deep-dive/src/app/dashboard/components/support-tickets/ticket/ticket.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, output, signal } from '@angular/core';
+import { Ticket } from '../support-ticket.smodel';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>({});
+  close = output();
+  detailsVisible = signal<boolean>(false);
 
+  onToggleDetails() {
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
+
+  onComplete() {
+    this.close.emit();
+  }
 }
