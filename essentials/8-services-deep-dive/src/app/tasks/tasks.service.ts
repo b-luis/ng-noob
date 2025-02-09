d79bff18c3471418dd203515fde3df9a64ce6e3c
b-luis <bscarletluis@gmail.com>
2025-02-09 15:58:20 +0800

    update(service): add addTask and updateTaskStaus and docs

---
+++ essentials/8-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -0,0 +1,36 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  //? Marking `tasks` as private ensures that it is encapsulated within the service,
+  //? preventing direct modification from outside components.
+  private tasks = signal<Task[]>([]);
+
+  //? Exposing `tasks` as a read-only signal allows components to observe changes
+  //? without directly mutating the data, maintaining controlled state management.
+  allTasks = this.tasks.asReadonly();
+
+  // public readonly tasksComputed = computed(() => this.tasks());
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+
+    //? [...oldTasks] - new copy so we dont mutate the old arr object in memory
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
+}
