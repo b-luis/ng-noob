9d24d61a6b73e8c25b0109a19c42fa550b364740
b-luis <bscarletluis@gmail.com>
2025-02-09 16:01:04 +0800

    update(tasks-list): inject tasks service to perform filtering on tasks()

--- essentials/8-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ essentials/8-services-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,8 +11,32 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
+  private tasksService: TasksService = inject(TasksService);
+
   selectedFilter = signal<string>('all');
-  tasks = [];
+
+  //? This binds `tasks` to the read-only `allTasks` signal from `TasksService`,
+  //? allowing the component to react to task updates while ensuring immutability of the original data.
+  // tasks = this.tasksService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService.allTasks().filter((task) => {
+          task.status === 'IN_PROGRESS';
+        });
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
