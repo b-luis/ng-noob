27a16c4bf3a4e5a84d02770d70a4f27c396feb9f
b-luis <bscarletluis@gmail.com>
2025-02-15 23:02:18 +0800

    chore: add docs on main.ts for root injector

--- essentials/8-services-deep-dive/src/main.ts
+++ essentials/8-services-deep-dive/src/main.ts
@@ -1,5 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
+
+//? providing a service on the entire application
+//? provider: piece of info that lets Angular know that a certain value should be injectible
+//? when u register this injectible value with the providers array, it wil lalways be
+//? included in the initial code bundle because Angular sees that it is needed right from the start
+// bootstrapApplication(
+//   AppComponent
+// {providers: [TasksService]},
+// ).catch((err) => console.error(err));
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
